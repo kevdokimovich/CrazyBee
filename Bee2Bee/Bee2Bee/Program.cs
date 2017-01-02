0822a8a38d00a7252172434555b0773cc0df6f61
Kati <Kati@KATI-HP>
2017-01-02 21:45:52 +0300

    Create project

---
+++ Bee2Bee/Bee2Bee/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bee2Bee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
